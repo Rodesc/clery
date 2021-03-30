e98e29d7c0a6e3ebefe4a9946cb9d9022282d2b9
RodescDev <rodolphe.deschaetzen@gmail.com>
2021-03-30 15:31:24 +0200

    component/Button: ActionButton added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,28 @@
+import { CSSProperties, FunctionComponent, MouseEventHandler } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Button.css';
 
 type buttonProps = {
 	value: string,
-	style?: any,
-	linkTo?: string
+	style?: CSSProperties,
+	linkTo?: string,
+	inactive?:boolean,
+	action?:MouseEventHandler
 }
 
-const Button = ({value, style}: buttonProps) => {
+const Button = ({value, style, inactive}: buttonProps) => {
 	return (
 		<button style={style} >{value}</button>
 	)
 }
 
-const LinkButton = ({value, style, linkTo}: buttonProps) => {
+// const Button = ({value, style, inactive}: buttonProps) => {
+// 	return (
+// 		<button style={style} >{value}</button>
+// 	)
+// }
+
+const LinkButton = ({value, style, linkTo, inactive}: buttonProps) => {
 	let goTo:string;
 	linkTo? goTo = linkTo : goTo = '';
 	console.log(goTo)
@@ -24,7 +33,14 @@ const LinkButton = ({value, style, linkTo}: buttonProps) => {
 	)
 }
 
+const ActionButton = ({value, style, inactive, action}: buttonProps) => {
+	return (
+		<button onClick={action} style={style} >{value}</button>
+	)
+}
+
 export {
     Button,
-    LinkButton
+    LinkButton,
+	ActionButton
 }
