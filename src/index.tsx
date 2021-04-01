7c20fd76fd4ed182fe33220f80203fe4cde856e6
RodescDev <rodolphe.deschaetzen@gmail.com>
2021-04-01 16:37:50 +0200

    routing handles login and upload and analysis page

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	RouteComponentProps,
+	Redirect
 } from "react-router-dom";
-
+import {Location} from 'history'
 import './index.css';
+
 import Auth from './screens/Auth';
+import Upload from './screens/Upload';
+import Analysis from './screens/Analysis';
+
 import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 
+const loggedIn = false;
+
 ReactDOM.render(
-  <Router>
-    <Header/>
+	<Router>
+		<Header loggedIn={loggedIn}/>
 
-    <Route path="/register" render={() => {
-      return <Auth showLogin={false}/>
-    }}/>
+		<Route path="/register" render={() => {
+			return <Auth showLogin={false}/>
+		}}/>
 
-    <Route path="/login" render={() => {
-      return <Auth showLogin={true}/>
-    }}/>
-    
-  </Router>,
-  document.getElementById('root')
+		<Route path="/login" render={() => {
+			return <Auth showLogin={true}/>
+		}}/>
+
+		<Route path="/upload" >
+			{loggedIn ? <Upload /> : <Redirect to="/" />}
+		</Route>
+
+		<Route path="/analysis" render={({location}:RouteComponentProps<{}, {}, any>) => {
+			if(!loggedIn) return <Redirect to="/" />
+			
+			if(location.state && location.state.file) 
+				return <Analysis file={location.state.file}/>
+			else return <Upload />
+
+			
+		}} />
+
+	</Router>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log());
+
