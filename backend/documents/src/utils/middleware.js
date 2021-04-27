76e57f55c91e0320a481943d397b2b8b52ab2dae
RodescDev <rodolphe.deschaetzen@gmail.com>
2021-04-27 22:33:02 +0200

    backend/documents: connection URI modified

--- backend/documents/src/utils/middleware.js
+++ backend/documents/src/utils/middleware.js
@@ -2,19 +2,15 @@ const util = require('util')
 const multer = require('multer')
 const GridFsStorage = require('multer-gridfs-storage')
 
-// const mongoURI = `mongodb://
-// 	${process.env.DB_USER}:
-// 	${process.env.DB_PASSWORD}@
-// 	${process.env.DB_HOST}:
-// 	${process.env.DB_PORT}/?authSource=admin`
-const mongoURI = `mongodb://localhost:${process.env.DB_PORT}`
+const mongoURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/admin`
+// const mongoURI = `mongodb://localhost:${process.env.DB_PORT}`
 
 var storage = new GridFsStorage({
 	url: mongoURI,
 	options: {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-		auth: { user: process.env.DB_USER, password: process.env.DB_PASSWORD },
+		// auth: { user: process.env.DB_USER, password: process.env.DB_PASSWORD },
 	},
 	file: (req, file) => {
 		console.log(file.originalname)
