07a98fcd3f593ee9a8023de19258b585e6ec4739
RodescDev <rodolphe.deschaetzen@gmail.com>
2021-04-27 22:24:53 +0200

    backend/boiler: added base source allow control

--- backend/boiler_js/src/index.js
+++ backend/boiler_js/src/index.js
@@ -2,15 +2,32 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.urlencoded()); // parse application/json
+app.use(express.urlencoded()) // parse application/json
 app.use(express.json())
 
 const port = process.env.port || 80
 
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	)
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,content-type'
+	)
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	next()
+})
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 // define a route handler for the default home page
 app.get('/status', (req, res) => {
 	console.log(`get request`)
-	res.status(200).send({status:'ok'})
+	res.status(200).send({ status: 'ok' })
 })
 
 // start the Express server
